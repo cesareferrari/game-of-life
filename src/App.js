475efbae024ba88e14119f9a03e1cad2f53b8f33
Cesare Ferrari <cesare@maryandferrari.com>
2020-06-19 14:18:30 -0400

    Create grid for display

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import React from "react";
 import "./App.css";
 import Grid from "./components/grid";
 
-function App() {
-  return (
-    <div className="App">
-      <Grid />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.rows = 30;
+    this.cols = 30;
+
+    this.state = {
+      generation: 0,
+      grid: Array(this.rows)
+        .fill()
+        .map(() => Array(this.cols).fill(false))
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Game of life</h1>
+        <Grid grid={this.state.grid} rows={this.rows} cols={this.cols} selectBox={this.selectBox} />
+
+        <p>Generation: {this.state.generation}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/grid.js
+++ src/components/grid.js
@@ -2,7 +2,31 @@ import React from "react";
 
 class Grid extends React.Component {
   render() {
-    return <h2>This is the grid</h2>;
+    const gridDisplay = [];
+
+    for (let i = 0; i < this.props.rows; i++) {
+      for (let j = 0; j < this.props.cols; j++) {
+
+        let boxClass = this.props.grid[i][j] ? "box on" : "box off"
+        let boxId = `box-${i}-${j}`
+
+        gridDisplay.push(
+          <Box
+            boxClass={boxClass}
+            key={boxId}
+            row={i}
+            col={j}
+            selectBox={this.props.selectBox}
+          />
+        )
+      }
+    }
+
+
+    return (
+      <div className="grid">
+      </div>
+    )
   }
 }
 
