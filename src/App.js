9303149f6ebecc24b9787ba981b3aeaad2a3c396
Cesare Ferrari <cesare@maryandferrari.com>
2020-06-19 13:03:11 -0400

    Add grid component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-// import Main from "./components/main"
+import React from "react";
+import "./App.css";
+import Grid from "./components/grid";
 
 function App() {
   return (
     <div className="App">
-        <Grid />
+      <Grid />
     </div>
   );
 }
---
+++ src/components/grid.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Grid extends React.Component {
+  render() {
+    return <h2>This is the grid</h2>;
+  }
+}
+
+export default Grid;
