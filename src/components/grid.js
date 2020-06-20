922916d00fae5b512db2d7665a92c64c1f9a20d3
Cesare Ferrari <cesare@maryandferrari.com>
2020-06-20 11:09:52 -0400

    Modify display grid

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Box from "./box";
 
 class Grid extends React.Component {
   render() {
@@ -6,27 +7,23 @@ class Grid extends React.Component {
 
     for (let i = 0; i < this.props.rows; i++) {
       for (let j = 0; j < this.props.cols; j++) {
-
-        let boxClass = this.props.grid[i][j] ? "box on" : "box off"
-        let boxId = `box-${i}-${j}`
+        let boxClass = this.props.grid[i][j] ? "box on" : "box off";
+        let boxId = `box-${i}-${j}`;
 
         gridDisplay.push(
           <Box
             boxClass={boxClass}
             key={boxId}
+            id={boxId}
             row={i}
             col={j}
             selectBox={this.props.selectBox}
           />
-        )
+        );
       }
     }
 
-
-    return (
-      <div className="grid">
-      </div>
-    )
+    return <div className="grid">{gridDisplay}</div>;
   }
 }
 
