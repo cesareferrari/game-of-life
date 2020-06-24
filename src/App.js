dbf137810cbc8ae52ec3b2e43ea64564d42b4af6
Cesare Ferrari <cesare@maryandferrari.com>
2020-06-23 22:30:23 -0400

    Add rules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Game from "./components/game";
+import Rules from "./components/rules";
 
 const App = () => {
   return (
     <div className="app">
       <Game />
+      <Rules />
     </div>
   )
 }
